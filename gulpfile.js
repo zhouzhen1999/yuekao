b74621d65505546953690fe2a46ec6b3ca2eec1c
zhenzhen <@2868382033.com>
2018-12-18 20:20:30 +0800

    起服务完成

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ let server = require("gulp-webserver");
 let fs = require("fs");
 let url = require("url");
 let path = require("path");
+let data = require("./src/mock/data.json");
+console.log(data)
 
 
 gulp.task('sass', function() {
@@ -11,3 +13,25 @@ gulp.task('sass', function() {
         .pipe(sass())
         .pipe(gulp.dest('./src/css'));
 });
+
+gulp.task("watch", function() {
+    return gulp.watch("./src/scss/*.scss", gulp.series("sass"))
+})
+
+gulp.task("server", function() {
+    return gulp.src("src")
+        .pipe(server({
+            port: 9009,
+            middleware: function(req, res, next) {
+                let pathname = url.parse(req.url).pathname;
+                if (pathname == "/api/list") {
+
+                } else if (pathname == "/favicon.ico") {
+                    return
+                } else {
+                    pathname = pathname == "/" ? "index.html" : pathname;
+                    res.end(fs.readFileSync(path.join(__dirname, "src", pathname)))
+                }
+            }
+        }))
+})
